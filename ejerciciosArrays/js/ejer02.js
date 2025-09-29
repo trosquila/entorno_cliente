850e58b65cc4c83f797b38b7ef0a2cf8f942da9f
SergioSalinas <salinassierrasergio05@email.com>
2025-09-29 09:47:34 +0200

    [add][mod] se cambio la organización de ficheros de arrays y se añadio el ejercicio02

---
+++ ejerciciosArrays/js/ejer02.js
@@ -0,0 +1,7 @@
+function ejer02(){
+    let arrayCostura =[];
+    arrayCostura.push("tela", "bies", "hilo", "tijeras", "máquina de coser", "botón");
+    arrayCostura.splice(1,1);
+    console.log(arrayCostura);
+}
+ejer02();
