ddeaed632f3b7614fae90d9fe3d2b1b8fe744022
SergioSalinas <salinassierrasergio05@email.com>
2025-10-22 14:08:12 +0200

    [ADD] ejercicio02 DOM

---
+++ DOM/ejercicio02/js/ejercicio02.js
@@ -0,0 +1,11 @@
+function ejer02(){
+    const div = document.getElementById('seccionPrimera');
+    const primerP = div.firstElementChild;
+    console.log(primerP);
+
+    const tercerP = div.getElementsByTagName('p')[2];
+    console.log(tercerP);
+    const ul = document.getElementsByTagName('ul')[0];
+    console.log(ul.lastElementChild);
+}
+ejer02();
