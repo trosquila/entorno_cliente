fa687276bcf1998d96c8cd5c97ea4951c0106526
SergioSalinas <salinassierrasergio05@email.com>
2025-11-20 10:06:43 +0100

    si

---
+++ GestionDeEventosyFormularios/ejerciciosRepaso/pdfExpresionesRegulares/ejer03/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let selectorColor = document.querySelector('input[type="color"]');
+    selectorColor.value = '#f6ff7a';
+    console.log();
+    
+})
