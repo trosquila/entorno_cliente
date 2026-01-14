9b05c302e57fa0604d33657a60ce29c9eeb1ee06
SergioSalinas <salinassierrasergio05@email.com>
2026-01-14 13:39:09 +0100

    ejercicios adicionales 0 finiquitao

--- vue/temaVue01/ejerciciosAdicionales/Ejercicio0/scripts/main.js
+++ vue/temaVue01/ejerciciosAdicionales/Ejercicio0/scripts/main.js
@@ -5,9 +5,9 @@ const app = Vue.createApp({
     let nombre = ref("Luke");
     let apellidos = ref("Skywalker");
     let edad = ref(20);
-    let imagen = ref("../img/imgEjemplo.jpg");
+    let imagen = ref("img/imgEjemplo.jpg");
     let textoImagen = ref("Foto de Luke Skywalker");
-    let estilos = ref(false);
+    let estilos = ref(true);
     let enlaces = ref([
       "https://www.starwars.com/",
       "https://starwars.fandom.com/es/wiki/Databank_(sitio_web)",
