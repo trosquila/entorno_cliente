784477faa66bc2e3e924c7966c42984125e39d00
Sergio Salinas <correoSergio@falso.com>
2026-01-25 17:49:24 +0100

    terminado adicionales

--- vue/temaVue01/ejerciciosAdicionales/ejercicio06/js/main.js
+++ vue/temaVue01/ejerciciosAdicionales/ejercicio06/js/main.js
@@ -5,11 +5,13 @@ const app = Vue.createApp({
         let formulario = ref({
             titulo: '',
             autor: '',
-            numEjemplares: 0
+            numEjemplares: 0,
+            prestados: 0
         });
-
+        let totalDisponible = ref(0);
+        let mediaLibros = ref(0);
         let memoriaDisponible = ref([]);
-        let memoriaPrestado = [];
+
         let guardarForm = () =>{
             let validar = true;
             if(formulario.value.titulo.trim() == ''){
@@ -25,31 +27,56 @@ const app = Vue.createApp({
                 alert('formulario incompleto');
             }
 
-
             if(validar == true){
-                const dato = {...formulario.value};
 
-                if(memoriaDisponible.value.length > 0){
+                const dato = {...formulario.value};
+                console.log('titulo '+ dato.titulo+' autor '+dato.autor);
+                
+                const buscarAutor = memoriaDisponible.value.some((element) => element.autor === dato.autor);
+                const buscarLibro = memoriaDisponible.value.some((element) => element.titulo === dato.titulo);
+
+                console.log(buscarAutor);
+                console.log(buscarLibro);
+                
+                if(buscarAutor == true && buscarLibro == true){
                     memoriaDisponible.value.forEach(element => {
-                        if(element.titulo == dato.titulo && element.autor == dato.autor){
+                        if(element.titulo == dato.titulo && element.nombre == dato.nombre){
                             element.numEjemplares += dato.numEjemplares;
-                        }else{
-                            memoriaDisponible.value.push(dato);
                         }
                     });
+                    
                 }else{
                     memoriaDisponible.value.push(dato);
                 }
-            }
-                    console.log('form '+ formulario.value);
-                    console.log('memoria '+ memoriaDisponible.value);
-        }
 
-        
+                totalDisponible.value += dato.numEjemplares;
+                mediaLibros.value = totalDisponible.value / memoriaDisponible.value.length;
+            }
+        }
+        let sacarLibro  = (index) =>{
+            if(memoriaDisponible.value[index].prestados == memoriaDisponible.value[index].numEjemplares){
+                alert('No quedan ejemplares para prestar');
+            }else{
+                memoriaDisponible.value[index].prestados += 1;
+            }
+            
+        }
+        let devolverLibro  = (index) =>{
+            if(memoriaDisponible.value[index].prestados == 0){
+                alert('Ya esta todo devuelto');
+            }else{
+                memoriaDisponible.value[index].prestados -= 1;
+            }
+            
+        }
         return {
             formulario,
             memoriaDisponible,
-            guardarForm
+            totalDisponible,
+            mediaLibros,
+            guardarForm, 
+            sacarLibro,
+            devolverLibro
         };
     },
 });
