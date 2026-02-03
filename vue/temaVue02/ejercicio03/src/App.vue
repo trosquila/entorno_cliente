9c057c5d1f812815871f1d5d2c9b1fb68a27964a
SergioSalinas <salinassierrasergio05@email.com>
2026-02-03 13:55:27 +0100

    borra lista

--- vue/temaVue02/ejercicio03/src/App.vue
+++ vue/temaVue02/ejercicio03/src/App.vue
@@ -7,13 +7,16 @@
       { texto: "Hacer deporte", hecho: false, activo: false },
       { texto: "Comer más sano", hecho: false, activo: false },
       { texto: "Viajar más", hecho: true, activo: false }]);
+      let borrarLista = () =>{
+        listaPropositos.value = [];
+      }
 </script>
 
 <template>
     <h1>Propositos de año nuevo:</h1>
     <ListaPropositos :propositos="listaPropositos"></ListaPropositos>
-    <NuevoProposito :propositos="listaPropositos">  </NuevoProposito>
-
+    <NuevoProposito :propositos="listaPropositos"></NuevoProposito>
+  <button type="button" @click="borrarLista">Borrar lista</button>
 </template>
 
 <style scoped></style>
