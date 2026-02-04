59d39e04d9f1b9be04e77d39771f61c9d03efb1d
SergioSalinas <salinassierrasergio05@email.com>
2026-02-04 14:09:15 +0100

    a

--- vue/temaVue02/ejercicio03/src/components/ListaPropositos.vue
+++ vue/temaVue02/ejercicio03/src/components/ListaPropositos.vue
@@ -6,10 +6,12 @@
 
 <template>
     <h2>lista</h2>
-    <ul v-if="propositos.length > 0">
-        <li v-for="(proposito, index) in propositos" :key="index" >
-            {{proposito.texto}}
-        </li>
-    </ul>
+    <ul v-if="propositos.length>0 && !propositos.every(prop=>prop.activo===true)">
+            <li v-for="proposito in propositos" :class="{esconder:proposito.activo}">
+                <p v-if="proposito.hecho===false" :class="{rojo:true}">{{proposito.texto}}</p>
+                <del v-else @dblclick="proposito.activo=true" :class="{verde:true}">{{proposito.texto}}</del>
+                <input type="checkbox" v-model="proposito.hecho">
+            </li>
+        </ul>
     <p v-else>La lista de porpositos está vacía</p>
 </template>
