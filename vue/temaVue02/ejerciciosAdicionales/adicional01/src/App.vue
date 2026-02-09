1a75d490ac3e52476391d3b14e33e4a1604915c9
SergioSalinas <salinassierrasergio05@email.com>
2026-02-09 10:57:42 +0100

    ejercicioAdicional01

---
+++ vue/temaVue02/ejerciciosAdicionales/adicional01/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Calendario from './components/Calendario.vue';
+import Citas from './components/Citas.vue';
+
+</script>
+
+<template>
+  <div class="contenedor">
+    <Calendario></Calendario>
+    <Citas></Citas>
+  </div>
+  
+</template>
+
+<style scoped>
+  .contenedor{
+    display: grid;
+  }
+</style>
---
+++ vue/temaVue02/ejerciciosAdicionales/adicional01/src/components/Calendario.vue
@@ -0,0 +1,77 @@
+<script setup>
+//obtener primerDiaMes
+const generarCalendario = () =>{
+    let calendario = [];
+
+    const mesFebrero = new Date(2021, 2, 1);
+    const primerDiaMes = mesFebrero.getDay();
+    const numeroDias = diasEnUnMes(mesFebrero.getMonth(), mesFebrero.getFullYear());
+    let dia = 0;
+    
+    for( let s = 0; s < 5; s++){
+        let semana = [];
+        for (let d = 0; d < 7; d++) {
+            if(dia < primerDiaMes || dia > numeroDias){
+                semana.push('');
+            }else{
+                semana.push(dia);
+            }
+            dia++;
+        }
+        calendario.push(semana);
+    }
+    return calendario;
+
+}
+
+function diasEnUnMes(mes, year) {
+    return new Date(year, mes, 0).getDate();
+}
+let calendario = generarCalendario();
+
+
+</script>
+<style>
+.contenedorTabla{
+    margin-top: 20px;
+}
+.calendario {
+    border-collapse: collapse;
+    width: 60%;
+}
+
+.calendario,
+th,
+td {
+    border: 1px solid black;
+    text-align: center;
+}
+
+th {
+    padding: 5px 10px;
+    background-color: rgb(255, 223, 182);
+}
+</style>
+<template>
+    <div class="contenedorTabla">
+        <table class="calendario">
+            <tr>
+                <th>Lunes</th>
+                <th>Martes</th>
+                <th>Miercoles</th>
+                <th>Jueves</th>
+                <th>Viernes</th>
+                <th>Sabado</th>
+                <th>Domingo</th>
+            </tr>
+            <tr v-for="(semana, index) in calendario" :key="index">
+
+                <td v-for="(dia, index) in semana" :key="index">
+                    {{ dia }}   
+                </td>
+
+            </tr>
+        </table>
+    </div>
+
+</template>
