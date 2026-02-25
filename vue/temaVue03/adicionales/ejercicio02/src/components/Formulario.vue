e9e81a97f0f5ae69006c603e74eb22532cb8b480
SergioSalinas <salinassierrasergio05@email.com>
2026-02-25 13:34:50 +0100

    w

---
+++ vue/temaVue03/adicionales/ejercicio02/src/components/Formulario.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const estadoBtn = ref(false);
+
+const memoriaNum = ref('');
+
+function validarBtn() {
+    if(memoriaNum.value){
+        estadoBtn.value = true;
+    }else{
+        estadoBtn.value = false;
+    }
+}
+</script>
+
+<template>
+    <form action="" method="post">
+        <div>
+            <label for="">Buscar por filtro</label>
+            <input type="number" name="nuevoNum" id="" @keyup="validarBtn()" v-model="memoriaNum">
+            <button type="submit" v-if="estadoBtn">Guardar</button>
+            <button type="submit" v-else disabled="">Guardar</button>
+        </div>
+    </form>
+</template>
--- vue/temaVue03/adicionales/ejercicio02/src/components/Menu.vue
+++ vue/temaVue03/adicionales/ejercicio02/src/components/Menu.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import Formulario from './Formulario.vue';
 </script>
 <template>
         <section class="menu">
                 <RouterLink to="/">Inicio</RouterLink>
                 <RouterLink to="/articulos">Articulos</RouterLink>
-                <RouterLink to="/acerca">Acerca de</RouterLink>        
+                <RouterLink to="/acerca">Acerca de</RouterLink>
+                <Formulario></Formulario>       
         </section>
         
 </template>
