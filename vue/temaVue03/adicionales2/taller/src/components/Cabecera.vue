a7be21772972312d4e0e627dea50ec31bbe65891
SergioSalinas <salinassierrasergio05@email.com>
2026-03-05 10:01:54 +0100

    w

---
+++ vue/temaVue03/adicionales2/taller/src/components/Cabecera.vue
@@ -0,0 +1,23 @@
+<script setup>
+import '../assets/styles/global.css';
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <header>
+        <h2>Talleres Hernández – Gestión Interna</h2>
+        <nav>
+            <div>
+                <RouterLink to="/panel">Panel</RouterLink>
+            </div>
+            <div>
+                <p>Vehículos</p>
+            </div>
+            <div>
+                <p>Reparaciones</p>
+            </div>
+            <div>
+                <p>Clientes</p>
+            </div>
+        </nav>
+    </header>
+</template>
---
+++ vue/temaVue03/adicionales2/taller/src/components/Pie.vue
@@ -0,0 +1,19 @@
+<script setup>
+import '../assets/styles/global.css';
+</script>
+<template>
+    <footer class="piePagina">
+        <div class="infoContacto">
+            <h3 class="tituloFooter">Talleres Hernández – Gestión Interna</h3>
+            <p>Calle Los Nogales 12, Salamanca</p>
+            <p>Tel: 923 000 000</p>
+            <p>Email: contacto@tallereshernandez.es</p>
+        </div>
+
+        <div class="redesSociales">
+            <a href="#" class="enlaceRed">Facebook</a>
+            <a href="#" class="enlaceRed">Instagram</a>
+            <a href="#" class="enlaceRed">Twitter</a>
+        </div>
+    </footer>
+</template>
